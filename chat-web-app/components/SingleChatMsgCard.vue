a590d9586b06eea9945b1730d6df2b0fef31a5db
Srushtika Neelakantam <n.srushtika@gmail.com>
2021-07-30 14:10:01 +0100

    Message metadata section added

--- chat-web-app/components/SingleChatMsgCard.vue
+++ chat-web-app/components/SingleChatMsgCard.vue
@@ -1,20 +1,19 @@
 <template>
   <div class="">
     <div
-      class="flex items-center"
+      class="flex items-baseline"
       :class="{
         'flex-row-reverse': isMsgMine
       }"
     >
-      <button
-        @click="editMyMsg(msgPayload.msg_id)"
-        v-if="isMsgMine && !didClickEdit"
-      >
-        <font-awesome-icon
-          class="mr-2 text-sm text-center text-white"
-          :icon="['fas', 'pencil-alt']"
-        />
-      </button>
+      <div v-if="isMsgMine && !didClickEdit">
+        <button @click="editMyMsg(msgPayload.msg_id)">
+          <font-awesome-icon
+            class="mr-2 text-sm text-center text-white"
+            :icon="['fas', 'pencil-alt']"
+          />
+        </button>
+      </div>
       <div v-if="isMsgMine && didClickEdit" class="flex flex-col">
         <button @click="saveEditedMsg()">
           <font-awesome-icon
@@ -29,16 +28,33 @@
           />
         </button>
       </div>
-      <div
-        class="rounded-xl w-8/12 m-4 p-3"
-        :class="{
-          'bg-gray-800': !isMsgMine,
-          'bg-purple-500': isMsgMine
-        }"
-      >
-        <p :contenteditable="didClickEdit" ref="editMsgPara">
-          {{ messageContents }}
-        </p>
+      <div class="w-8/12 m-4">
+        <div
+          class="rounded-xl p-3"
+          :class="{
+            'bg-gray-800': !isMsgMine,
+            'bg-purple-500': isMsgMine
+          }"
+        >
+          <p :contenteditable="didClickEdit" ref="editMsgPara">
+            {{ messageContents }}
+          </p>
+        </div>
+        <div
+          class="text-xs px-3 py-1 flex items-center"
+          :class="{
+            'flex-row-reverse': isMsgMine
+          }"
+        >
+          by {{ msgPayload.username }}
+          <template v-if="!isMsgFromToday">
+            on {{ date }}/{{ month }}
+          </template>
+          <template v-if="isMsgFromToday">
+            today
+          </template>
+          at {{ readableTimestamp }}&bull; edited &bull;
+        </div>
       </div>
     </div>
   </div>
@@ -46,14 +62,17 @@
 
 <script>
 import { mapGetters, mapActions, mapMutations } from "vuex";
-
 export default {
   props: ["msgPayload"],
   data() {
     return {
       isMsgMine: "",
       editedChatMsg: null,
-      didClickEdit: false
+      didClickEdit: false,
+      isMsgFromToday: null,
+      date: null,
+      month: null,
+      year: null
     };
   },
   computed: {
@@ -65,6 +84,19 @@ export default {
       } else {
         return originalMessage;
       }
+    },
+    readableTimestamp() {
+      const messageTime24HrFormat = new Date(
+        this.msgPayload.created_at_timestamp
+      );
+      if (!(this.isMsgFromToday = this.isToday(messageTime24HrFormat))) {
+        this.date = messageTime24HrFormat.getDate();
+        this.month = messageTime24HrFormat.getMonth() + 1;
+        this.year = messageTime24HrFormat.getFullYear();
+      }
+      let hours = messageTime24HrFormat.getHours();
+      let minutes = messageTime24HrFormat.getMinutes();
+      return this.convertTo12HrFormat(hours, minutes);
     }
   },
   methods: {
@@ -80,11 +112,28 @@ export default {
     saveEditedMsg() {
       this.editedChatMsg = this.$refs.editMsgPara.textContent;
       this.didClickEdit = false;
+    },
+    isToday(msgDate) {
+      const today = new Date();
+      if (
+        today.getDate() == msgDate.getDate() &&
+        today.getMonth() == msgDate.getMonth() &&
+        today.getFullYear() == msgDate.getFullYear()
+      ) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    convertTo12HrFormat(hours, minutes) {
+      minutes = (minutes < 10 ? "0" : "") + minutes;
+      let ampm = hours < 12 ? "am" : "pm";
+      hours = hours % 12 || 12;
+      return `${hours}:${minutes} ${ampm} `;
     }
   },
   created() {
     this.isMsgMine = this.msgPayload.client_id === this.getMyClientId;
-    console.log(this.msgPayload.client_id, this.getMyClientId, this.isMsgMine);
   }
 };
 </script>
